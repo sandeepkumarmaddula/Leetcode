41cffbb7c0b614ddc6e8248856f064723b28dbfb
sandeepkumarmaddula <sandeepmaddula31@gmail.com>
2026-07-21 11:29:01 +0530

    Time: 2 ms (10.84%), Space: 19.3 MB (67.09%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num==0: return "0"
+        neg=num<0
+        num=abs(num)
+        s=[]
+        while num>0:
+            s.append(str(num%7))
+            num//=7
+        if neg:
+            s.append("-")
+        return "".join(s[::-1])
