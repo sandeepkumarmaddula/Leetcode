8b86c6b2ed79220e3fe8ad682cf95135d35a292b
sandeepkumarmaddula <sandeepmaddula31@gmail.com>
2026-07-27 12:41:34 +0530

    Time: 813 ms (20.69%), Space: 29.8 MB (17.51%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.py
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.py
@@ -1,17 +1,11 @@
 class Solution:
     def maximumPrimeDifference(self, nums: List[int]) -> int:
         lis=[]
-        for i in range(len(nums)):
-            if nums[i]<2: continue
-            t=True
-            for j in range(2,int(nums[i]**0.5)+1):
-                if nums[i]%j==0:
-                    t=False
-                    break
-            if t:
-                lis.append(i)
-        if len(lis)<=1:
-            return 0
-        else:
-            return lis[-1]-lis[0]
+        for i,x in enumerate(nums):
+            if x>=2:
+                for j in range(2,int(x**0.5)+1):
+                    if nums[i]%j==0: break
+                else: lis.append(i)
+        if len(lis)<=1: return 0
+        else: return lis[-1]-lis[0]
 
